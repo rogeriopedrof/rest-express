945884bec35b243101d5033250bc04d7cb3ec820
rogeriopedrof <rogeriopedro.aa@gmail.com>
2026-01-22 16:40:12 -0300

    init Service + Correção e Adição no Controller

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,7 +1,6 @@
-import e, { request, response } from "express";
-import { updateTaskService } from '../services/taskService';
+import { updatedTaskService } from '../services/taskService';
 
-export async function updateTaskController (req: request, res: response) {
+export async function updatedTaskController (req, res) {
     const {taskId } = req.params;
     const data = req.body;
 
@@ -10,20 +9,24 @@ export async function updateTaskController (req: request, res: response) {
     }
 
     try {
-        const updateTask = await updateTaskService({
+        const updatedTask = await updatedTaskService({
             taskId,
             data,
         });
 
-        return res.status(200).json(updateTask)
+        return res.status(200).json(updatedTask)
         
-    } catch (error: any) {
+    } catch (error) {
         if (error.type === "NOT_FOUND") {
             return res.status(404).json({error: error.message});
         }
 
         if (error.type === "FORIDDEN") {
             return res.status(403).json({error: error.message});
+        }
+
+        if (error.type === "INVALID_STATE") {
+            return res.status(409).json({error: error.message});
         }
 
         return res.status(500).json({error: 'Internal server error'});
---
+++ src/services/taskService.js
@@ -0,0 +1,24 @@
+import { getTaskById, updatedTask } from '../repositories/taskRepository.js'
+
+export async function updatedTaskService({taskId, data}) {
+    
+    const task = await getTaskById(taskId);
+
+    if (!taskId) {
+        throw {
+            type: "NOT_FOUND",
+            message: "Task not found",
+        };
+    }
+
+    if (task.status === "COMPLETED") {
+        throw {
+            type: "INVALID_STATE",
+            message: "Completed task cannot be edited",
+        }
+    }
+
+    const updatedTask = await updatedTask(taskId, data);
+
+    return updatedTask;
+}
